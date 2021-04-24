43365abafa1c8d4b67bb9da552a349ef60e17c58
Artem-Rudko <72283896+Artem-Rudko@users.noreply.github.com>
2021-04-24 11:32:20 +0300

    add correction to timer.js from mentor, create two methods instead

--- timer.js
+++ timer.js
@@ -5,7 +5,7 @@ class CountdownTimer  {
         this.hours = document.querySelector(selector +' [data-value="hours"]'),
         this.mins = document.querySelector(selector +' [data-value="mins"]'),
         this.secs = document.querySelector(selector +' [data-value="secs"]'),
-        this.intervalId = null;       
+        this.intervalId = null;  
     };
 
     intervalId = setInterval(() => {
@@ -18,22 +18,29 @@ class CountdownTimer  {
             return
         }
 
-        const days = this.pad(Math.floor(time / (1000 * 60 * 60 * 24)));
-        const hours = this.pad(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
-        const mins = this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
-        const secs = this.pad(Math.floor((time % (1000 * 60)) / 1000));
-     
-        this.days.textContent = days;
-        this.hours.textContent = hours;
-        this.mins.textContent = mins;
-        this.secs.textContent = secs;
-        
+        this.timerRender(this.calculateTime(time));
     }, 1000);
     
     pad(value) {
         return String(value).padStart(2, '0')
     };
-}
+    
+    calculateTime(time) {
+        const days = this.pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+        const hours = this.pad(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
+        const mins = this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+        const secs = this.pad(Math.floor((time % (1000 * 60)) / 1000));
+
+        return {days,hours,mins,secs};
+    }
+
+    timerRender({days,hours,mins,secs}){
+        this.days.textContent = `${days}`;
+        this.hours.textContent = `${hours}`;
+        this.mins.textContent = `${mins}`;
+        this.secs.textContent = `${secs}`;
+    }
+};
 
 new CountdownTimer({
     selector: '#timer-1',
